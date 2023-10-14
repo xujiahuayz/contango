957641ade90030335fbbbf800c1554f04b62f205
JX <jx@JXs-MacBook-Pro.local>
2023-10-14 16:49:21 +0100

    add plots

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,55 +1,41 @@
-from perp.constants import DATA_PATH, SYMBOL_LIST
+from perp.constants import FIGURE_PATH, SYMBOL_LIST
 from scripts.simluate import c_perp_position_change
 
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+products = ["dYdX", "Huobi", "Binance", "Contango"]
 
 for symbol in SYMBOL_LIST:
-    print(symbol)
     coinglass_aave_df = c_perp_position_change(
         risk_asset=symbol, usd_asset="USDC", long_risk=True, leverage_multiplier=5
     )
-    sum_df = (
-        (
-            (
-                coinglass_aave_df[
-                    [
-                        # "Bitmex",
-                        "Binance",
-                        "Bybit",
-                        "OKX",
-                        "Huobi",
-                        "Gate",
-                        "Bitget",
-                        "dYdX",
-                        "CoinEx",
-                        # "BingX",
-                        "Contango",
-                    ]
-                ].iloc[1:-1]
-            )
-            * 100
-        )  # convert to percentage
-        .describe()
-        .T.sort_values("std", ascending=True)[
-            ["std", "mean", "min", "25%", "50%", "75%", "max", "count"]
-        ]
+
+    coinglass_aave_df = coinglass_aave_df[products] * 100
+
+    # plot with large font size
+
+    plt.rcParams.update({"font.size": 20})
+    fig, ax = plt.subplots()
+
+    # Plot the data
+    for product in products:
+        ax.plot(coinglass_aave_df.index, coinglass_aave_df[product], label=product)
+
+    # Add a legend on top of the plot outside the frame without border
+    ax.legend(ncol=2, loc="upper center", bbox_to_anchor=(0.5, 1.3), frameon=False)
+
+    # Format the x-axis to show only the month without year
+    ax.xaxis.set_major_formatter(mdates.DateFormatter("%b"))
+
+    # save to file
+    plt.savefig(
+        FIGURE_PATH / f"funding_rates_{symbol}.pdf",
+        bbox_inches="tight",
+        pad_inches=0,
+        transparent=True,
     )
 
-    sum_df["count"] = sum_df["count"].astype(int)
-    print(sum_df)
-
-
-# plt.plot(coinglass_aave_df["dYdX"], label="dYdX")
-
-# plt.plot(coinglass_aave_df["OKX"], label="OKX")
-# plt.plot(coinglass_aave_df["Binance"], label="Binance")
-
-# plt.plot(coinglass_aave_df["Contango"], label="Contango")
-# plt.legend()
-
-
-plt.plot(coinglass_aave_df["cperp_health"])
-# draw a horizontal line at 1
-plt.axhline(y=1, color="r", linestyle="-")
-
-coinglass_aave_df.to_excel(DATA_PATH / "coinglass_aave_df.xlsx")
+    # Show the plot
+    plt.show()
+    plt.close()
